836f4fb800c3969d5413c1871cfea27be451ca93
BigCorps <interpag.bigcorps@gmail.com>
2025-05-18 20:54:13 -0300

    Create page.tsx

---
+++ apps/meutypebot/page.tsx
@@ -0,0 +1,10 @@
+// app/meutypebot/page.tsx
+export default function MeuTypebotPage() {
+  return (
+    <iframe
+      src="https://typebot.co/inicio-6mwxdka"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      allow="camera; microphone; autoplay"
+    />
+  );
+}
