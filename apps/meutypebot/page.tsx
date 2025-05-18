f1767f5b5b66a9fc4760831aab622e6b1f07ee33
BigCorps <interpag.bigcorps@gmail.com>
2025-05-18 20:56:49 -0300

    Update page.tsx

--- apps/meutypebot/page.tsx
+++ apps/meutypebot/page.tsx
@@ -2,7 +2,7 @@
 export default function MeuTypebotPage() {
   return (
     <iframe
-      src="https://typebot.co/inicio-6mwxdka"
+      src="https://viewer.typebot.io/inicio-6mwxdka"
       style={{ width: "100%", height: "100vh", border: "none" }}
       allow="camera; microphone; autoplay"
     />
