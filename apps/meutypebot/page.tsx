bf62d44fe7618e35734b0331230dd08001a65ab3
BigCorps <interpag.bigcorps@gmail.com>
2025-05-18 21:09:30 -0300

    Update page.tsx
    Adiciona rota /meutypebot com iframe do Typebot

--- apps/meutypebot/page.tsx
+++ apps/meutypebot/page.tsx
@@ -1,14 +1,9 @@
-// app/meutypebot/page.tsx
 export default function MeuTypebotPage() {
   return (
-    <html lang="pt-BR">
-      <body style={{ margin: 0 }}>
-        <iframe
-          src="https://viewer.typebot.io/inicio-6mwxdka"
-          style={{ width: "100%", height: "100vh", border: "none" }}
-          allow="camera; microphone; autoplay"
-        />
-      </body>
-    </html>
+    <iframe
+      src="https://viewer.typebot.io/inicio-6mwxdka"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      allow="camera; microphone; autoplay"
+    />
   );
 }
